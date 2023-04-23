6995578328b2eed0e6a7c06640aa2d5c10ac70cb
Saatvik Arya <aryasaatvik@gmail.com>
2023-04-22 22:47:25 -0700

    ui

--- src/Tool.tsx
+++ src/Tool.tsx
@@ -1,38 +1,28 @@
 import React, { memo, useCallback, useEffect } from "react";
 import { useGlobals, useStorybookApi } from "@storybook/manager-api";
 import { Icons, IconButton } from "@storybook/components";
-import { ADDON_ID, PARAM_KEY, TOOL_ID } from "./constants";
+import { PARAM_KEY, TOOL_ID } from "./constants";
 
 export const Tool = memo(function MyAddonSelector() {
   const [globals, updateGlobals] = useGlobals();
-  const api = useStorybookApi();
 
-  const isActive = [true, "true"].includes(globals[PARAM_KEY]);
+  const isActive = globals[PARAM_KEY];
 
-  const toggleMyTool = useCallback(() => {
+  const toggleCookies = useCallback(() => {
     updateGlobals({
       [PARAM_KEY]: !isActive,
     });
   }, [isActive]);
 
-  useEffect(() => {
-    api.setAddonShortcut(ADDON_ID, {
-      label: "Toggle Measure [O]",
-      defaultShortcut: ["O"],
-      actionName: "outline",
-      showInMenu: false,
-      action: toggleMyTool,
-    });
-  }, [toggleMyTool, api]);
 
   return (
     <IconButton
       key={TOOL_ID}
       active={isActive}
-      title="Enable my addon"
-      onClick={toggleMyTool}
+      title={isActive ? "Disable Cookies" : "Enable Cookies"}
+      onClick={toggleCookies}
     >
-      <Icons icon="lightning" />
+      <Icons icon="database" />
     </IconButton>
   );
 });
--- src/components/PanelContent.tsx
+++ src/components/PanelContent.tsx
@@ -1,76 +1,63 @@
 import React, { Fragment } from "react";
 import { styled, themes, convert } from "@storybook/theming";
-import { TabsState, Placeholder, Button } from "@storybook/components";
-import { List } from "./List";
+import { TabsState, Button, Icons } from "@storybook/components";
+import { CookieList } from "./CookieList";
 
-export const RequestDataButton = styled(Button)({
-  marginTop: "1rem",
+export const Actions = styled.div({
+  position: "sticky",
+  top: 0,
+  padding: "1rem",
+  display: "flex",
+  gap: "1rem",
 });
 
-type Results = {
-  danger: any[];
-  warning: any[];
-};
-
 interface PanelContentProps {
-  results: Results;
-  fetchData: () => void;
-  clearData: () => void;
 }
+
+const cookies = [
+  {
+      "domain": ".github.com",
+      "expirationDate": 1699595438.68171,
+      "hostOnly": false,
+      "httpOnly": false,
+      "name": "_octo",
+      "path": "/",
+      "sameSite": "lax",
+      "secure": true,
+      "session": false,
+      "value": "test",
+      "id": 1
+  }
+]
 
 /**
  * Checkout https://github.com/storybookjs/storybook/blob/next/code/addons/jest/src/components/Panel.tsx
  * for a real world example
  */
-export const PanelContent: React.FC<PanelContentProps> = ({
-  results,
-  fetchData,
-  clearData,
-}) => (
+export const PanelContent: React.FC<PanelContentProps> = () => (
   <TabsState
-    initial="overview"
+    initial="cookiesList"
     backgroundColor={convert(themes.normal).background.hoverable}
   >
+    
     <div
-      id="overview"
-      title="Overview"
-      color={convert(themes.normal).color.positive}
-    >
-      <Placeholder>
-        <Fragment>
-          Addons can gather details about how a story is rendered. This is panel
-          uses a tab pattern. Click the button below to fetch data for the other
-          two tabs.
-        </Fragment>
-        <Fragment>
-          <RequestDataButton
-            secondary
-            small
-            onClick={fetchData}
-            style={{ marginRight: 16 }}
-          >
-            Request data
-          </RequestDataButton>
-
-          <RequestDataButton outline small onClick={clearData}>
-            Clear data
-          </RequestDataButton>
-        </Fragment>
-      </Placeholder>
-    </div>
-    <div
-      id="danger"
-      title={`${results.danger.length} Danger`}
-      color={convert(themes.normal).color.negative}
-    >
-      <List items={results.danger} />
-    </div>
-    <div
-      id="warning"
-      title={`${results.warning.length} Warning`}
-      color={convert(themes.normal).color.warning}
-    >
-      <List items={results.warning} />
+      id="cookiesList"
+      title="Cookies">
+      <Fragment>
+        <Actions>
+          <Button secondary small>
+            <Icons icon="delete" />
+            Clear Cookies
+          </Button>
+          <Button secondary small>
+            <Icons icon="add" />
+            Add Cookie
+          </Button>
+          <Button secondary small>Import Cookies</Button>
+          <Button secondary small>Export Cookies</Button>
+        </Actions>
+        <CookieList cookies={cookies} />
+      </Fragment>
     </div>
   </TabsState>
 );
